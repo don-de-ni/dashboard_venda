485ffb75bef451c683dbdd71bc5c9e5fb379cf55
don-de-ni <138634906+don-de-ni@users.noreply.github.com>
2025-03-13 21:20:41 -0300

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
 import pandas as pd
-import plotly as px
+import plotly.express as px
 
 def formata_numero(valor, prefixo=''):
     for unidade in ['', 'mil']:
